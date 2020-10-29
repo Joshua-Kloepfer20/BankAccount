c78395f27510a549eb81197505d2eef4d642ed80
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2020-10-28 23:39:10 -0400

    I made the class with the variables, the constructer and 2 of the functions

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+  public BankAccount(int accID, String pass) {
+    balance = 0;
+    accountID = accID;
+    password = pass;
+  }
+  public double getBalance() {
+    return balance;
+  }
+  public int accountID() {}
+}
